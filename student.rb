f288dfda9bc9f75a91fc38231881c93168fd4d3a
Olawale <olapetanwale@outlook.com>
2022-08-25 20:57:28 +0100

    Add relatinships between books, rentals, classroom, student and person objects

--- student.rb
+++ student.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
-require './person'
+require_relative './person'
 
 # Student class create the students object
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(classroom, age, name = 'Unknown')
     super(age, name)
     @classroom = classroom
@@ -12,7 +14,13 @@ class Student < Person
   def play_hooky
     "¯\(ツ)/¯"
   end
+
+  def add_classroom(classroom)
+    @classroom = classroom
+    @classroom.students.push(self) unless classroom.students.include?(self)
+  end
+
 end
 
-wale = Student.new('Physics', 20, 'John')
-p wale.can_use_service?
+john = Student.new('Physics', 20, 'John')
+p john.add_classroom()
