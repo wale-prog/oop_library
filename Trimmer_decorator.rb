d762906b1b330c9657d6228c41fab97e857abba0
Olawale Olapetan <olapetanwale@outlook.com>
2022-08-25 13:48:45 +0100

    Delete Trimmer_decorator.rb

--- Trimmer_decorator.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require './decorator'
-# Implement the Trimmer decorator class
-class TrimmerDecorator < Decorator
-  def correct_name
-    @nameable.correct_name.slice(0..9)
-  end
-end
